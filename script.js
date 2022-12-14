60567e48a41f8905c8171b43689f0c263312e8f2
KarinaK17 <117162205+KarinaK17@users.noreply.github.com>
2022-12-14 17:13:41 +0300

    make a boilerplate in index.html and write functions for basic math operators

---
+++ script.js
@@ -0,0 +1,31 @@
+const add = function (a, b) {
+    return a+b;
+};
+
+const subtract = function(a, b) {
+    return a-b;
+};
+
+const multiply = function(a, b) {
+    return a*b;
+  };
+
+  const divide = function(a, b) {
+    if (b=0) {
+        return "OOPS! You can't divide by '0'.";
+    } else {
+        return a/b;
+    }
+  };
+
+  const operate = function(operator, a, b) {
+    if (operator=="+") {
+        add(a,b);
+    } else if (operator=="-") {
+        subtract(a,b);
+    } else if (operator=="*") {
+        multiply(a,b);
+    } else if (operator=="/") {
+        divide(a,b);
+    } 
+  }
